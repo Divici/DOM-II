23330c53c54cc2558a21e5fe8b874fb48e27e6a0
david-aihe <doa9200@gmail.com>
2021-11-30 18:19:36 -0500

    added double click event on images

--- src/index.js
+++ src/index.js
@@ -1,25 +1,44 @@
 import './less/index.less'
 
 // on click event
+
 const signupButton = document.querySelectorAll('.content-pick .btn');
 
 signupButton.forEach(btn => {
     btn.addEventListener('click', (event)=>{
         btn.textContent = 'Thank you!';
     }) 
- });
+});
 
  // on mouseover and mouseout
 
  const links = document.querySelectorAll('nav a');
  
- links.forEach(link => {
+links.forEach(link => {
     link.addEventListener('mouseover', event =>{
         link.style.color = 'red';
     })
     link.addEventListener('mouseout', event =>{
         link.style.color = '';
     })
- });
+});
 
- 
+// double click
+
+const images = document.querySelectorAll('.img-content img');
+
+const shrink = (event, img) => {
+    img.style.width = '50%';
+}
+
+images.forEach(img =>{
+    img.addEventListener('dblclick', (event) => shrink(event, img))
+});
+
+// zoom on wheel
+
+
+
+
+
+// key press down
